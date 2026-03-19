bc660a60ccc23ba0f4c35c95cb08bc04205c8add
HanleyKingston <hkingsto@gmail.com>
2026-03-19 08:54:19 -0700

    add decode script for chunk 9

---
+++ workshop_scripts/decode_chunk.py
@@ -0,0 +1,17 @@
+
+str9_encrypted = open("..\\encrypted_data\\9.txt").read()
+
+def decrypt(encrypted_message:str, key:int) -> str:
+    message = ""
+    for i in range(len(encrypted_message)):
+        char = encrypted_message[i]
+        decrypted_char = chr((ord(char) - key) % 126)
+        message += decrypted_char
+    return message
+
+
+str9_decrypted = decrypt(encrypted_message = str9_encrypted, key = 9)
+
+with open("..\\workshop_data\\9.txt", "w") as file:
+    file.write(str9_decrypted)
+
